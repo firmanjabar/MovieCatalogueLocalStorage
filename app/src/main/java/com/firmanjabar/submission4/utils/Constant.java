6064e41b14022fee16252daef34a5d84e92fa5c9
FirmanJabar <firmanabduljabar@gmail.com>
2019-09-09 16:00:03 +0800

    stack widget fix

--- app/src/main/java/com/firmanjabar/submission4/utils/Constant.java
+++ app/src/main/java/com/firmanjabar/submission4/utils/Constant.java
@@ -5,7 +5,7 @@ import com.firmanjabar.submission4.BuildConfig;
 public class Constant {
     public final static String API_KEY = BuildConfig.MOVIE_API_KEY;
     public final static String BASE_URL = BuildConfig.BASE_URL;
-    final static String BASE_POSTER_URL = BuildConfig.BASE_POSTER_URL;
+    public final static String BASE_POSTER_URL = BuildConfig.BASE_POSTER_URL;
     final static String BASE_BACKDROP_URL = BuildConfig.BASE_BACKDROP_URL;
     public final static String YOUTUBE_WATCH = BuildConfig.YOUTUBE_WATCH;
 
